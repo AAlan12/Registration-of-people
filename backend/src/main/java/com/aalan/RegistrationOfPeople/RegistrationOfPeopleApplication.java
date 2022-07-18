e90dbf9a3a2583ddb86fa62f433c549e1abb221f
Antonio Alan <tonio.alan07@gmail.com>
2022-07-18 16:19:51 -0300

    First commit

---
+++ backend/src/main/java/com/aalan/RegistrationOfPeople/RegistrationOfPeopleApplication.java
@@ -0,0 +1,13 @@
+package com.aalan.RegistrationOfPeople;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationOfPeopleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrationOfPeopleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/aalan/RegistrationOfPeople/RegistrationOfPeopleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aalan.RegistrationOfPeople;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrationOfPeopleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
